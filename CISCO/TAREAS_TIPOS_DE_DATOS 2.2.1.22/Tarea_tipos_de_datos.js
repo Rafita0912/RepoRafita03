276631976dae8c945a97fa4552bc26586824c661
RepoRafita03.git <rafael.antequera@gmail.com>
2023-08-02 19:33:47 -0400

    tareas Rafael Antequera

---
+++ CISCO/TAREAS_TIPOS_DE_DATOS 2.2.1.22/Tarea_tipos_de_datos.js
@@ -0,0 +1,35 @@
+let variableBoolean = true
+let variableNumber = 12345
+let variableBigInt = 1234n
+let variableString = "Rafael"
+let variableUndefined =
+console.log("formato de ", variableBoolean, "es un Boolean")
+console.log("formato de ", variableNumber, "es un Number")
+console.log("formato de ", variableBigInt, "es un BigInt")
+console.log("formato de ", variableString, "es un String")
+console.log("formato de ", variableUndefined, "es una variable Undefined")
+console.log("formato de ", variableBoolean, "es un Boolean", "formato de ", 
+variableNumber, "es un Number", "formato de ", 
+variableBigInt, "es un BigInt","formato de ", 
+variableString, "es un String", "formato de ", 
+variableUndefined, "es una variable Undefined")
+
+let varString = "1234"
+let varNumber = (Number(varString))
+let varBigInt = (BigInt(varNumber))
+console.log(varString)
+console.log(varNumber)
+console.log(varBigInt)
+
+let var1 = "Rafa" + "  cadena"
+let var2 = 123+345
+let var3 = 234n+345n
+let var4 = true+true
+let var5 = varNumber+var2+var1
+const str1=42+Number("1")
+console.log(var1)
+console.log(var2)
+console.log(var3)
+console.log(var4)
+console.log(var5)
+console.log(str1)
