dd29cbf5bde1efee42f1d5fdec5fd74b92850110
RepoRafita03.git <rafael.antequera@gmail.com>
2023-08-04 15:09:18 -0400

    tareas Rafael Antequera

---
+++ CISCO/TAREA INTERACCION CON EL USUARIO 3.3.1.7/Tarea.js
@@ -0,0 +1,12 @@
+let ancho = prompt("Para calcular el volumen de una caja, proporciona el ancho en mt.", 0);
+let alto = prompt("Para calcular el volumen de una caja, proporciona el alto en mt. ", 0);
+let largo = prompt("Para calcular el volumen de una caja, proporciona el largo en mt. ", 0);
+volumenCaja = ancho * alto * largo;
+console.log("El volumen de la caja es : ", volumenCaja)
+alert('El volumemn de la caja es de ${volumenCaja} metros cubicos ')
+
+let width = prompt("Volumen de la caja, ingresa el ancho", 0);
+let height = prompt("Volumen de la caja, ingresa la altura", 0);
+let length = prompt("Volumen de la caja, ingresa la longitud", 0);
+let volume = width * height * length;
+alert(`El volumen de la caja es ${volume}`);
