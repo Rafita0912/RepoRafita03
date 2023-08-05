8f7bd798ad587d9ef3a0644efd8f12a15bcf1acd
RepoRafita03.git <rafael.antequera@gmail.com>
2023-08-04 20:06:13 -0400

    tareas Rafael Antequera

--- CISCO/LABORATORIO INTERACCION 3.3.1.8/Laboratorio.js
+++ CISCO/LABORATORIO INTERACCION 3.3.1.8/Laboratorio.js
@@ -2,23 +2,25 @@ let contacts = [{
     name: "Maxwell Wright",
     phone: "(0191) 719 6495",
     email: "egestas@nonummyac.co.uk"
-    }, {
+}, {
     name: "Raja Villarreal",
     phone: "0866 398 2895",
     email: "posuere@sed.com"
-    }, {
+}, {
     name: "Helen Richards",
     phone: "0800 1111",
     email: "libero@vallis.edu"
-    }];
-    
-    // escribe tu código aquí
-    
-    contacts[length].name = prompt("introduzca nuevo nombre de contacto : ", "nombre")
-    contacts[length].phone = prompt("introduzca nuevo telefono de contacto : ", "telefono")
-    contacts[length].email = prompt("introduzca nuevo email de contacto : ", "email")
-    
-    let last = contacts.length - 1;
-    
-    console.log(`${contacts[0].name} / ${contacts[0].phone} / ${contacts[0].email}`);
-    console.log(`${contacts[last].name} / ${contacts[last].phone} / ${contacts[last].email}`);
+}];
+
+// escribe tu código aquí
+
+let tam = contacts.length;
+
+contacts[tam].name = prompt("introduzca nuevo nombre de contacto : ", "nombre");
+contacts[tam].phone = prompt("introduzca nuevo telefono de contacto : ", "telefono");
+contacts[tam].email = prompt("introduzca nuevo email de contacto : ", "email");
+
+let last = contacts.length - 1;
+
+console.log(`${contacts[0].name} / ${contacts[0].phone} / ${contacts[0].email}`);
+console.log(`${contacts[last].name} / ${contacts[last].phone} / ${contacts[last].email}`);
---
+++ CISCO/LABORATORIO INTERACCION 3.3.1.8/tempCodeRunnerFile.js
@@ -0,0 +1,19 @@
+let contacts = [{
+    name: "Maxwell Wright",
+    phone: "(0191) 719 6495",
+    email: "egestas@nonummyac.co.uk"
+}, {
+    name: "Raja Villarreal",
+    phone: "0866 398 2895",
+    email: "posuere@sed.com"
+}, {
+    name: "Helen Richards",
+    phone: "0800 1111",
+    email: "libero@vallis.edu"
+}];
+
+// escribe tu código aquí
+
+let tam = contacts.length;
+
+let a = prompt("introduzca nuevo nombre de contacto : ", "nombre");
