b12e1fa37a93e4d8ebdb7d5ecd723bea240750ca
RepoRafita03.git <rafael.antequera@gmail.com>
2024-04-11 17:56:28 -0400

    abriendo 2 ventanas y generando secuencia para las otras

--- TRABAJOS-PYTHON/COTIZA_ALU_TEM/tempCodeRunnerFile.py
+++ TRABAJOS-PYTHON/COTIZA_ALU_TEM/tempCodeRunnerFile.py
@@ -4,6 +4,7 @@ import inicializa
 from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox 
+#Python image Library
 from PIL import ImageTk, Image
 
 import sqlite3
@@ -19,7 +20,7 @@ class Registro:
 
     db_name='database_proyecto.db'
     
-    def __init__(self, ventana):
+    def __init__(self,vetana):
         self.window=ventana   
         self.window.title("REGISTRO DE DATOS")
         self.window.geometry("393x580")
@@ -179,20 +180,18 @@ class Registro:
             query='INSERT INTO Usuarios VALUES(NULL, ?, ?, ?, ?, ?, ?, ?, ?)'
             parameters = (self.obra.get(),self.cliente.get(),self.telefono.get(),self.direccion.get(),self.correo.get(),self.observaciones.get(),self.password.get())
             self.Limpiar()
-            self.window.destroy()
-            Opciones()
 
-
-
+"""
+PANTALLA DE OPCIONES
+"""
 class Opciones:
 
     db_name='database_proyecto.db'
-    ventana=Tk()
     
-    def __init__(self, ventana):
+    def __init__(self,vetana):
         self.window=ventana   
         self.window.title("REGISTRO DE DATOS")
-        self.window.geometry("400x780")
+        self.window.geometry("450x780")
         self.window.resizable(0,0)
         self.window.config(bd=10)
         
@@ -201,12 +200,6 @@ class Opciones:
         titulo= Label(ventana, text="ELIGE LA OPCION A COTIZAR",fg="black",font=("Comic Sans", 13,"bold"),pady=5).pack()
 
         "--------------- Logo e imagenes ------------"
-        imagen_registro=Image.open("C:/Users/PcAsusZenbookRafita/Desktop/REPO_RAFAEL_ANTEQUERA/TRABAJOS-PYTHON/COTIZA_ALU_TEM/imagenes/logo_masercon.png")
-        nueva_imagen=imagen_registro.resize((240,60))
-        render=ImageTk.PhotoImage(nueva_imagen)
-        label_imagen= Label(ventana, image= render)
-        label_imagen.image=render
-        label_imagen.pack(pady=3)
 
         imagen_registro=Image.open("C:/Users/PcAsusZenbookRafita/Desktop/REPO_RAFAEL_ANTEQUERA/TRABAJOS-PYTHON/COTIZA_ALU_TEM/imagenes/frentes_fijos.png")
         nueva_imagen=imagen_registro.resize((400,100))
@@ -222,9 +215,9 @@ class Opciones:
         label_imagen.image=render
         label_imagen.pack(pady=3)
 
-        "--------- Elija el cerramiento a cotizar ------------"
+        "--------- Marco para toma de datos ------------"
 
-        marco = LabelFrame(ventana, text="Cerramiento elegido",font=("Comic Sans", 10,"bold"))
+        marco = LabelFrame(ventana, text="Registro de datos",font=("Comic Sans", 10,"bold"))
         marco.config(bd=2,pady=3)
         marco.pack()
 
@@ -246,6 +239,11 @@ class Opciones:
         self.telefono.grid(row=2, column=1, padx=10, pady=3)
         te = self.telefono
 
+       # label_sexo=Label(marco,text="Sexo: ",font=("Comic Sans", 10,"bold")).grid(row=3,column=0,sticky='s',padx=10,pady=3)
+       # self.combo_sexo=ttk.Combobox(marco,values=["Masculino", "Femenino"], width=22,state="readonly")
+       # self.combo_sexo.current(0)
+       # self.combo_sexo.grid(row=3,column=1,padx=10,pady=3)
+
         label_direccion=Label(marco,text="Dirección : ",font=("Comic Sans", 10,"bold")).grid(row=3,column=0,sticky='s',padx=10,pady=3)
         self.direccion=Entry(marco,width=25)
         self.direccion.grid(row=3, column=1, padx=10, pady=3)
@@ -266,6 +264,26 @@ class Opciones:
         self.password.grid(row=6, column=1, padx=10, pady=3)
         con = self.password
 
+        #label_password=Label(marco,text="Repetir contraseña: ",font=("Comic Sans", 10,"bold")).grid(row=7,column=0,sticky='s',padx=10,pady=8)
+        #self.repetir_password=Entry(marco,width=25,show="*")
+        #self.repetir_password.grid(row=7, column=1, padx=10, pady=8)
+        
+        #"--------------- Marco pregunta --------------------"
+        #marco_pregunta = LabelFrame(ventana, text="Si olvidas tu contraseña",font=("Comic Sans", 10,"bold"),pady=10)
+        #marco_pregunta.config(bd=2,pady=5)
+        #marco_pregunta.pack()
+        #"--------------- Pregunta --------------------"
+        #label_pregunta=Label(marco_pregunta,text="Pregunta: ",font=("Comic Sans", 10,"bold")).grid(row=0,column=0,sticky='s',padx=10,pady=8)
+        #self.combo_pregunta=ttk.Combobox(marco_pregunta,values=["¿Nombre de tu primera mascota?","¿Lugar dónde fuiste al colegio?","¿En que ciudad naciste?","¿Cómo se llama tu equipo favorito?"], width=30,state="readonly")
+        #self.combo_pregunta.current(0)
+        #self.combo_pregunta.grid(row=0,column=1,padx=10,pady=8)
+  
+        #label_respuesta=Label(marco_pregunta,text="Respuesta: ",font=("Comic Sans", 10,"bold")).grid(row=1,column=0,sticky='s',padx=10,pady=8)
+        #self.respuesta=Entry(marco_pregunta,width=33)
+        #self.respuesta.grid(row=1, column=1, padx=10, pady=8)        
+        
+        #label_nota=Label(marco_pregunta,text="*Esta respuesta te permitira recuperar tu contraseña.",font=("Comic Sans", 9,"bold"),foreground="blue").grid(row=2,column=0,columnspan=2,sticky='s',padx=10)
+
         "--------------- Frame botones --------------------"
 
         frame_botones=Frame(ventana)
@@ -274,9 +292,16 @@ class Opciones:
         "------------------ Botones -----------------------"
         
         boton_registrar=Button(frame_botones,text="REGISTRAR Y COTIZAR",command=self.Registrar_usuario ,height=2,width=20,bg="green",fg="white",font=("Comic Sans", 10,"bold")).grid(row=0, column=1, padx=10, pady=15)
-
+        #boton_limpiar=Button(frame_botones,text="LIMPIAR",command=self.Limpiar_formulario ,height=2,width=10,bg="gray",fg="white",font=("Comic Sans", 10,"bold")).grid(row=0, column=2, padx=10, pady=15)
         boton_cancelar=Button(frame_botones,text="CERRAR",command=ventana.quit ,height=2,width=10,bg="red",fg="white",font=("Comic Sans", 10,"bold")).grid(row=0, column=3, padx=10, pady=15)
-                   
+        
+    # def Ejecutar_consulta(self, query, parameters=()):
+    #    with sqlite3.connect(self.db_name) as conexion:
+    #        cursor=conexion.cursor()
+    #        result=cursor.execute(query,parameters)
+    #        conexion.commit()
+    #    return result 
+              
     def Validar_formulario_completo(self):
         if len(self.obra.get()) !=0 and len(self.cliente.get()) !=0 and len(self.telefono.get()) !=0 and len(self.direccion.get()) !=0 and len(self.correo.get()) !=0 and len(self.observaciones.get()) !=0 and len(self.password.get()) !=0:
             return True
@@ -291,17 +316,41 @@ class Opciones:
         self.correo.delete(0, END)
         self.observaciones.delete(0, END)
         self.password.delete(0, END)
+    #    self.repetir_password.delete(0, END)
+    #    self.combo_pregunta.delete(0, END)
+    #    self.respuesta.delete(0, END)
+     
+ 
+    # def Buscar_dni(self, dni):
+    #    with sqlite3.connect(self.db_name) as conexion:
+    #        cursor=conexion.cursor()
+    #        sql="SELECT * FROM Usuarios WHERE DNI = {}".format(dni)
+    #        cursor.execute(sql)
+    #        dnix= cursor.fetchall() # obtener respuesta como lista
+    #        cursor.close()
+    #        return dnix
+    
+    # def Validar_dni(self):
+    #    dni= self.dni.get()
+    #    dato = self.Buscar_dni(dni)
+    #    if (dato == []):
+    #        return True
+    #    else:
+    #        messagebox.showerror("ERROR EN REGISTRO", "DNI registrado anteriormente")
 
     def Registrar_usuario(self):
         if self.Validar_formulario_completo() and self.Validar_contraseña():
             query='INSERT INTO Usuarios VALUES(NULL, ?, ?, ?, ?, ?, ?, ?, ?)'
             parameters = (self.obra.get(),self.cliente.get(),self.telefono.get(),self.direccion.get(),self.correo.get(),self.observaciones.get(),self.password.get())
             self.Limpiar() 
-    ventana.mainloop()
 
+
+              
 if __name__ == '__main__':
     ventana=Tk()
     application=Registro(ventana)
+    input()
+    application=Opciones(ventana)
     ventana.mainloop()
 
 
